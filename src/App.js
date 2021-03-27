2df182fb4685a28ac8288f72a7ce0436de446dcd
Patricio Castro <luckydrawings@gmail.com>
2021-03-27 21:55:24 +0100

    global css added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <h1>Drones</h1>
+    <>
+      <h1>Drones</h1>
+      <button className='btn'>See Report</button>
+    </>
   );
 }
 
